2d403cc9b38d360c6bcf4b0b458b22df29a6dbd4
anne-kd <51865410+anne-kd@users.noreply.github.com>
2021-02-15 08:49:13 +0100

    added dataSource

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarConfig, DayC, Day } from  'material-calendar';
+
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.css']
 })
+
+
+
 export class CalenderComponent implements OnInit {
 
-  constructor() { }
+      calendarConfig: CalendarConfig = {
+      renderMode: 'monthly', // 'annual' | 'monthly'
+      selectMode: 'range',  // 'click' | 'range'
+      displayYear: false,
+      firstDayOfWeekMonday: true,
+      calendarWeek: true,
+      switches: true,
+    }
+
+  dataSource: DayC[] = [
+    {
+      date: 1604185200000,
+      backgroundColor: 'rgba(0, 0, 255, 0.5)',
+      toolTip: 'Test ToolTip',
+      badgeMode: 'Icon',
+      badgeInt: 5,
+      badgeIcon: 'edit'
+    },
+    {
+      date: 1604199900000,
+      backgroundColor: 'blue'
+    }
+  ]
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  testMethod(){
+    console.log('hallo');
+  }
 }
