4a60f6b6e68fa186ec5ed7f38b96af28c5d60619
Kristin Zenger <kristin.zenger@t-online.de>
2021-02-22 11:57:45 +0100

    Replacing icon and adapting cache
    The icon in the Browser bar is now the right one. Furthermore are all the icons cached. Theme colors have been changed as well.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-event-planer';
+
+  /*
+  install() {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(User response to the install prompt: ${outcome});
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+  };
+  */
 }
+
+
