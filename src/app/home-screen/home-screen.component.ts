0415e06c7adb112050cdb49e325a800e6bae8f3d
anne-kd <51865410+anne-kd@users.noreply.github.com>
2021-02-25 16:49:39 +0100

    install button on homescreen

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home-screen',
@@ -10,6 +11,41 @@ export class HomeScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  //adding a customized install button
+  //code snippet (adapted) from https://levelup.gitconnected.com/pwa-add-button-to-home-screen-and-remote-debugging-in-android-devices-with-angular-8-3dbaec772a1
+  deferredPrompt: any;
+  showButton = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  install() {
+      // hide A2HS button
+      this.showButton = false;
+      // Show the prompt
+      this.deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+        //if the user decides to install the button disappears
+        const btnInstall = document.getElementById("btnInstall");
+        btnInstall.style.display = "none";
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      this.deferredPrompt = null;
+    });
   }
 
 }
