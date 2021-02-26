d87cabfee0b33ec794d8bdb6741ffdf63d567ac8
liviamxhk <lmaxhaku@gmail.com>
2021-02-26 17:59:57 +0100

    add web-push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { WebNotificationService } from './shared/web-notification.service';
 
 
 @Component({
@@ -11,22 +12,36 @@ import { SwUpdate } from '@angular/service-worker';
 
 export class AppComponent implements OnInit {
   title = 'my-event-planer';
+  permission: NotificationPermission | null = null;
 
+  constructor(
+    private swUpdate: SwUpdate,
+    private webNotificationService: WebNotificationService
+    ) {}
 
-  constructor(private swUpdate: SwUpdate) {}
+    ngOnInit() {
+      // ...
+      this.permission = this.webNotificationService.isEnabled ? Notification.permission : null;
+    }
+
+    submitNotification() {
+      this.webNotificationService.subscribeToNotifications()
+        .then(() => this.permission = Notification.permission);
+    }
 
   //informing the user if a newer version is available;
   //code snippet (adapted) from https://angular-buch.com/blog/2019-07-progressive-web-app
-  ngOnInit() {
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe((e) => {
-        const updateAvailability = window.confirm(`
-          Update available!
-          Do you want to install it?
-        `);
-        if (updateAvailability) { window.location.reload(); }
-      });
-    }
-  }
+
+  // ngOnInit() {
+  //  if (this.swUpdate.isEnabled) {
+  //    this.swUpdate.available.subscribe((e) => {
+  //      const updateAvailability = window.confirm(`
+  //        Update available!
+  //        Do you want to install it?
+  //      `);
+  //      if (updateAvailability) { window.location.reload(); }
+  //    });
+  //  }
+ // }
 
 }
---
+++ src/app/web-notification.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SwPush } from '@angular/service-worker';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebNotificationService {
+  readonly VAPID_PUBLIC_KEY = 'BGk2Rx3DEjXdRv9qP8aKrypFoNjISAZ54l-3V05xpPOV-5ZQJvVH9OB9Rz5Ug7H_qH6CEr40f4Pi3DpjzYLbfCA';
+  private baseUrl = 'https://api3.angular-buch.com/notifications';
+
+
+  constructor(
+    private http: HttpClient,
+    private swPush: SwPush
+  ) { }
+  get isEnabled() {
+    return this.swPush.isEnabled;
+}
+subscribeToNotifications(): Promise<any> {
+  return this.swPush.requestSubscription({
+    serverPublicKey: this.VAPID_PUBLIC_KEY
+  })
+  .then(sub => this.sendToServer(sub))
+  .catch(err => console.error('Could not subscribe to notifications', err));
+}
+
+private sendToServer(params: PushSubscriptionJSON) {
+  this.http.post(this.baseUrl, params).subscribe();
+}
+}
